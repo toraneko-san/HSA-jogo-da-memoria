c5fa0ef94a7d2c70636af0d6f528e9a37fb74e52
toraneko-san <mykarakida@gmail.com>
2025-08-02 00:43:44 -0300

    randomizar cartas

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
-// 
+import { PARES } from "./const.js";
+
+const cartasEmJogo = [];
+
+function iniciarJogo() {
+  // esconder seção "inicio" e revelar seção "carta-ampliada"
+  ////
+}
+
+// quando clicar no botão "Jogar", chamar função iniciarJogo()
+////
+
+function carregarJogadores() {
+  // inserir nome e pontuação inicial dos jogadores na seção "jogadores" e
+  // atualizar pontuação durante o jogo
+  ////
+}
+
+function embaralharCartas() {
+  // se precisar limitar o número de cartas, fazer aqui
+  // P.S.: randomizar a ordem dos pares
+  ////
+
+  // separar cada par em duas cartas
+  for (let i = 0; i < PARES.length; i++) {
+    const par = PARES[i];
+
+    cartasEmJogo.push({ texto: par.carta1, parId: i });
+    cartasEmJogo.push({ texto: par.carta2, parId: i });
+  }
+
+  // randomizar a ordem das cartas
+  for (let i = cartasEmJogo.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [cartasEmJogo[i], cartasEmJogo[j]] = [cartasEmJogo[j], cartasEmJogo[i]];
+  }
+}
+
+function carregarTabuleiro() {
+  embaralharCartas();
+
+  // inserir cartas no tabuleiro
+  ////
+}
+
+// preparar jogo quando o site carregar
+carregarJogadores();
+carregarTabuleiro();
