87aafe8ac442e66e225ae47400e2eb391a151ec5
toraneko-san <mykarakida@gmail.com>
2025-08-12 10:55:05 -0300

    adicionar imagens

--- const.js
+++ const.js
@@ -2,18 +2,19 @@ export const PARES = [
   {
     texto: "Arquitetura Hostil",
     imagem: "arquitetura-hostil.png",
-    referencia: "Reprodução",
+    referencia:
+      "https://www.cnnbrasil.com.br/politica/bolsonaro-veta-lei-padre-julio-lancellotti-que-proibe-construcoes-hostis-a-populacao-de-rua/",
   },
   // {
   //   texto: "Serviços Ecossistêmicos",
   //   imagem: "servico-ecossistemico.jpg",
   //   referencia: ""
   // },
-  // {
-  //   texto: "Redescentralizar São Paulo",
-  //   imagem: "redescentralizar.png",
-  //   referencia: ""
-  // },
+  {
+    texto: "Redescentralização",
+    imagem: "redescentralizacao.jpg",
+    referencia: "Joaquín Torres García (1878-1949)",
+  },
   // {
   //   texto: "(I)mobilidade Urbana",
   //   imagem: "img imobilidade-urbana",
@@ -27,27 +28,27 @@ export const PARES = [
   {
     texto: "Medicalização",
     imagem: "medicalizacao.jpg",
-    referencia: "",
+    referencia: "https://diplomatique.org.br/medicalizacao-da-experiencia-humana/",
   },
   {
     texto: "Cidadania Insurgente e Transgressiva",
     imagem: "cidadania-insurgente-transgressiva.jpg",
     referencia:
-      "Nós, mulheres da periferia (2017) – https://nosmulheresdaperiferia.com.br/no-grajau-sarau-discute-diversidade-sexual-das-mulheres-na-quebrada",
+      "Nós, mulheres da periferia – https://nosmulheresdaperiferia.com.br/no-grajau-sarau-discute-diversidade-sexual-das-mulheres-na-quebrada",
   },
   {
     texto: "Resistência e Resiliência",
     imagem: "resistencia-resiliencia.png",
     referencia: "",
   },
-  // {
-  //   texto: "Segregação territorial",
-  //   imagem: "img segregacao-territorial",
-  //   referencia: ""
-  // },
-  // {
-  //   texto: "?",
-  //   imagem: "img ?",
-  //   referencia: ""
-  // },
+  {
+    texto: "Segregação territorial",
+    imagem: "segregacao-territorial.jpg",
+    referencia: "https://123ecos.com.br/docs/segregacao-espacial/",
+  },
+  {
+    texto: "Saúde global",
+    imagem: "saude-global.png",
+    referencia: "",
+  },
 ];
