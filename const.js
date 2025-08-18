419e121b5a152f5df82019367cb31cb32015e1d8
toraneko-san <mykarakida@gmail.com>
2025-08-18 15:53:24 -0300

    consertar tamanho da carta semi ampliada

--- const.js
+++ const.js
@@ -15,65 +15,65 @@ export const PARES = [
     imagem: "redescentralizacao.jpg",
     referencia: "Desenho de Joaquín Torres García (1943)",
   },
-  {
-    texto: "(I)mobilidade Urbana",
-    imagem: "imobilidade-urbana.jpg",
-    referencia: "Foto tirada pela jornalista Rovena Rosa/Agência Brasil"
-  },
-  {
-    texto: "Abandono Social",
-    imagem: "abandono-social.jpg",
-    referencia: "Fotografia do jornalista Mauro Sampaio no livro 'Abandono' (2020)"
-  },
-  {
-    texto: "Medicalização",
-    imagem: "medicalizacao.jpg",
-    referencia: "https://diplomatique.org.br/medicalizacao-da-experiencia-humana/",
-  },
-  {
-    texto: "Cidadania Insurgente e Transgressiva",
-    imagem: "cidadania-insurgente-transgressiva.jpg",
-    referencia:
-      "Nós, mulheres da periferia – https://nosmulheresdaperiferia.com.br/no-grajau-sarau-discute-diversidade-sexual-das-mulheres-na-quebrada",
-  },
-  {
-    texto: "Resistência e Resiliência",
-    imagem: "resistencia-resiliencia.png",
-    referencia: "https://sebraepr.com.br/comunidade/artigo/resiliencia-em-tempos-de-crise",
-  },
-  {
-    texto: "Segregação territorial",
-    imagem: "segregacao-territorial.jpg",
-    referencia: "https://123ecos.com.br/docs/segregacao-espacial/",
-  },
-  {
-    texto: "Saúde global",
-    imagem: "saude-global.png",
-    referencia: "https://ciclovivo.com.br/vida-sustentavel/bem-estar/cientistas-pedem-grande-transicao-pela-saude-global/",
-  },
-  {
-    texto: "11",
-    imagem: "11.png",
-    referencia: ""
-  },
-  {
-    texto: "11",
-    imagem: "11.png",
-    referencia: ""
-  },
-  {
-    texto: "11",
-    imagem: "11.png",
-    referencia: ""
-  },
-  {
-    texto: "11",
-    imagem: "11.png",
-    referencia: ""
-  },
-  {
-    texto: "11",
-    imagem: "11.png",
-    referencia: ""
-  },
+  // {
+  //   texto: "(I)mobilidade Urbana",
+  //   imagem: "imobilidade-urbana.jpg",
+  //   referencia: "Foto tirada pela jornalista Rovena Rosa/Agência Brasil"
+  // },
+  // {
+  //   texto: "Abandono Social",
+  //   imagem: "abandono-social.jpg",
+  //   referencia: "Fotografia do jornalista Mauro Sampaio no livro 'Abandono' (2020)"
+  // },
+  // {
+  //   texto: "Medicalização",
+  //   imagem: "medicalizacao.jpg",
+  //   referencia: "https://diplomatique.org.br/medicalizacao-da-experiencia-humana/",
+  // },
+  // {
+  //   texto: "Cidadania Insurgente e Transgressiva",
+  //   imagem: "cidadania-insurgente-transgressiva.jpg",
+  //   referencia:
+  //     "Nós, mulheres da periferia – https://nosmulheresdaperiferia.com.br/no-grajau-sarau-discute-diversidade-sexual-das-mulheres-na-quebrada",
+  // },
+  // {
+  //   texto: "Resistência e Resiliência",
+  //   imagem: "resistencia-resiliencia.png",
+  //   referencia: "https://sebraepr.com.br/comunidade/artigo/resiliencia-em-tempos-de-crise",
+  // },
+  // {
+  //   texto: "Segregação territorial",
+  //   imagem: "segregacao-territorial.jpg",
+  //   referencia: "https://123ecos.com.br/docs/segregacao-espacial/",
+  // },
+  // {
+  //   texto: "Saúde global",
+  //   imagem: "saude-global.png",
+  //   referencia: "https://ciclovivo.com.br/vida-sustentavel/bem-estar/cientistas-pedem-grande-transicao-pela-saude-global/",
+  // },
+  // {
+  //   texto: "11",
+  //   imagem: "11.png",
+  //   referencia: ""
+  // },
+  // {
+  //   texto: "11",
+  //   imagem: "11.png",
+  //   referencia: ""
+  // },
+  // {
+  //   texto: "11",
+  //   imagem: "11.png",
+  //   referencia: ""
+  // },
+  // {
+  //   texto: "11",
+  //   imagem: "11.png",
+  //   referencia: ""
+  // },
+  // {
+  //   texto: "11",
+  //   imagem: "11.png",
+  //   referencia: ""
+  // },
 ];
